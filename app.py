ed06ab22b69422d257046b947207ca805855498e
satishnaga1 <48474935+satishnaga1@users.noreply.github.com>
2025-09-20 12:42:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 from flask import Flask, render_template, request
+from datetime import datetime
 import requests
 
 app = Flask(__name__)
 
-API_KEY = "ef9a3d31837bf5a5ea1f5085a43aab92"  # Replace with your OpenWeatherMap API key
+# Replace with your OpenWeatherMap API key
+API_KEY = "ef9a3d31837bf5a5ea1f5085a43aab92"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     weather_data = None
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
     if request.method == "POST":
         city = request.form["city"]
-        response = requests.get(f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric")
+        response = requests.get(
+            f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric")
         if response.status_code == 200:
             weather_data = response.json()
         else:
             weather_data = {"error": "City not found!"}
 
-    return render_template("index.html", weather_data=weather_data)
+    return render_template("index.html", weather_data=weather_data, current_time=current_time)
 
 
 if __name__ == "__main__":
